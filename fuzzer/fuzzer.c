573b5cddb47b40338642243e627ab8463c1ff898
antrikshdhand <>
2024-01-09 10:59:08 +1100

    Add string-free fuzzer implementation

---
+++ fuzzer/fuzzer.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "grammar.h"
+
+// #define DEBUG 1
+#define ITERATIONS 10000
+
+/**
+ * @brief Tests if the given key is a non-terminal by checking if the MSB is
+ * set. If so, the function returns the index of the non-terminal in the
+ * Grammar `non_terminals` array.
+ * 
+ * @param key The key to be verified as non-terminal.
+ * @param grammar The grammar struct representing a BNF grammar.
+ * @return int The index of the non-terminal inside the grammar, otherwise -1.
+ */
+int is_non_terminal(Token key, Grammar* grammar) 
+{
+    // Check if MSB is set.
+    if ((key & 0x80) == 0x80) {
+        // If the key is a non-terminal, the index will be the least
+        // significant byte.
+        return key & 0x0F;
+    }
+
+    return -1;
+
+    // for (int i = 0; i < grammar->num_non_terminals; i++)
+    // {
+    //     NonTerminal nt = grammar->non_terminals[i];
+    //     if (nt.name == key) {
+    //         return i;
+    //     }
+    // }
+}
+
+void unify_key_inv(Token key, Grammar* grammar, TokenArray* fuzzed)
+{
+    #ifdef DEBUG
+    printf("\nKey: 0x%x\n", key);
+    #endif
+
+    size_t nt_index;
+    if ((nt_index = is_non_terminal(key, grammar)) != -1)
+    {
+        // Get the non-terminal related to the given key.
+        NonTerminal nt = grammar->non_terminals[nt_index];
+
+        // Pick a random rule associated with the non-terminal.
+        size_t rand_rule_index = rand() % nt.num_rules;
+        Rule rand_rule = nt.rules[rand_rule_index];
+
+        #ifdef DEBUG
+        printf("It's non-terminal.\n");
+        printf("rand_rule_index: %zu\n", rand_rule_index);
+        printf("Random rule num tokens: %zu\n", rand_rule.num_tokens);
+        printf("Random rule 0th token: 0x%x\n", rand_rule.tokens[0]);
+        #endif
+
+        // Generate some Tokens from that Rule.
+        unify_rule_inv(rand_rule, grammar, fuzzed);
+    }
+    else
+    {
+        #ifdef DEBUG
+        printf("It's a terminal symbol.\n");
+        #endif
+
+        // The only string which a terminal symbol can generate is 
+        // the symbol itself.
+        fuzzed->tokens[fuzzed->index] = key;
+        fuzzed->index++;
+    }
+}
+
+void unify_rule_inv(Rule rule, Grammar* grammar, TokenArray* fuzzed)
+{
+    // For each token of the given rule, generate some terminal strings 
+    // by running `unify_key_inv` on it.
+
+    for (int i = 0; i < rule.num_tokens; i++)
+    {
+        Token token = rule.tokens[i];
+        unify_key_inv(token, grammar, fuzzed);
+    }
+}
+
+void print_token_array(TokenArray* fuzzed) 
+{
+    for (int i = 0; i < fuzzed->index; i++)
+    {
+        printf("0x%x\n", fuzzed->tokens[i]);
+    }
+}
+
+int main() 
+{
+    // Ensures randomness when selecting rule in non-terminal
+    srand(time(0));
+
+    /* MEASURE ALGORITHM TIME */
+    clock_t start_time, end_time;
+    double cpu_time_used;
+
+    start_time = clock();
+
+    for (int i = 0; i < ITERATIONS; i++)
+    {
+        TokenArray fuzzed;
+        fuzzed.index = 0;
+
+        Token start_token = 0x80;
+        unify_key_inv(start_token, &sample_grammar, &fuzzed);
+        // print_token_array(&fuzzed);
+    }
+
+    end_time = clock();
+    cpu_time_used = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
+    printf("Execution time (.c): %f seconds\n", cpu_time_used);
+
+}
---
+++ fuzzer/grammar.h
@@ -0,0 +1,154 @@
+#include <stdint.h>
+#include <stddef.h>
+
+#ifndef GRAMMAR_H
+#define GRAMMAR_H
+
+#define MAX_TOKENS_IN_RULE 1024 
+#define MAX_RULES_FOR_NONTERMINAL 1024
+#define MAX_NONTERMINALS_IN_GRAMMAR 128
+#define MAX_STRINGS_IN_LANGUAGE 100
+
+/**
+ * A Grammar is an array of NonTerminals. 
+ * A NonTerminal has a name (an 8-bit id), and an array of rules.
+ * A Rule consists of several Tokens e.g. "<article> <noun>".
+ * The Tokens are written in 8-bit form.
+ */
+
+typedef uint8_t Token;
+
+typedef struct TokenArray {
+    size_t index;
+    Token tokens[MAX_STRINGS_IN_LANGUAGE];
+} TokenArray;
+
+typedef struct Rule
+{
+    size_t num_tokens;
+    Token tokens[MAX_TOKENS_IN_RULE];
+} Rule;
+
+typedef struct NonTerminal
+{
+    Token name;
+    size_t num_rules;
+    Rule rules[MAX_RULES_FOR_NONTERMINAL];
+} NonTerminal;
+
+typedef struct Grammar
+{
+    size_t num_non_terminals;
+    NonTerminal non_terminals[MAX_NONTERMINALS_IN_GRAMMAR];
+} Grammar;
+
+// Note that the NonTerminals are listed in ascending order. This is important
+// to allow for O(1) lookup of index in the fuzzing algorithm.
+Grammar sample_grammar = {
+    // Number of non-terminals
+    6,
+
+    // Array of NonTerminals
+    {
+        // NonTerminal 1
+        {
+            // Name of NonTerminal
+            0x80,
+            
+            // Number of rules NonTerminal has
+            1,
+
+            // Array of Rules
+            {
+                // Rule 1
+                {
+                    // Number of Tokens in Rule
+                    1, 
+
+                    // Array of Tokens
+                    {0x81}
+                }
+            }
+        },
+        // NonTerminal 2
+        {
+            0x81,
+            1,
+            {
+                {
+                    2,
+                    {0x82, 0x83}
+                }
+            }
+        },
+        // NonTerminal 3
+        {
+            0x82,
+            1,
+            {
+                {
+                    2,
+                    {0x84, 0x85}
+                }
+            }
+        },
+        // NonTerminal 4
+        {
+            0x83,
+            3,
+            {
+                {
+                    1,
+                    {0x05}
+                },
+                {
+                    1,
+                    {0x06}
+                },
+                {
+                    1,
+                    {0x07}
+                }
+            }
+        },
+        // NonTerminal 5
+        {
+            0x84,
+            2,
+            {
+                {
+                    1,
+                    {0x03}
+                },
+                {
+                    1,
+                    {0x04}
+                }
+            }
+        },
+        // NonTerminal 6
+        {
+            0x85,
+            3,
+            {
+                {
+                    1,
+                    {0x00}
+                },
+                {
+                    1,
+                    {0x01}
+                },
+                {
+                    1,
+                    {0x02}
+                }
+            }
+        }
+    }
+};
+
+void unify_key_inv(Token key, Grammar* grammar, TokenArray* fuzzed);
+void unify_rule_inv(Rule rule, Grammar* grammar, TokenArray* fuzzed);
+
+#endif
